aa8ae43fdaf33205e657b781562de5a7e5046f59
Amal Jose <amal446446@gmail.com>
2025-09-12 13:48:37 +0530

    small ui change

--- src/components/magicui/animated-list.tsx
+++ src/components/magicui/animated-list.tsx
@@ -13,13 +13,13 @@ import React, {
 export function AnimatedListItem({ children }: { children: React.ReactNode }) {
     const animations = {
         initial: { scale: 0, opacity: 0 },
-        animate: { scale: 1, opacity: 1, originY: 0 },
+        whileInView: { scale: 1, opacity: 1, originY: 0 },
         exit: { scale: 0, opacity: 0 },
         transition: { type: "spring", stiffness: 350, damping: 40 },
     }
 
     return (
-        <motion.div {...animations} layout className="mx-auto w-full">
+        <motion.div {...animations} viewport={{once: true, amount: 0.3}} layout className="mx-auto w-full">
             {children}
         </motion.div>
     )
